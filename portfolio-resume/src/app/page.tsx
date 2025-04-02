813fd96d4ed7c237666c35bd5d416161bff83b01
ced888 <cedricdeleon70@gmail.com>
2025-04-01 23:39:35 -0600

    add content

--- portfolio-resume/src/app/page.tsx
+++ portfolio-resume/src/app/page.tsx
@@ -80,14 +80,14 @@ export default function Home() {
             translateZ="50"
             className="text-xl font-bold text-neutral-600 dark:text-white"
           >
-            Make things float in air
+            Cedric De Leon
           </CardItem>
           <CardItem
             as="p"
             translateZ="60"
             className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
           >
-            Hover over this card to unleash the power of CSS perspective
+            Software Developer
           </CardItem>
           <CardItem
             translateZ="50"
