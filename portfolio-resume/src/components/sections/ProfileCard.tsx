c285c158d860d08ba16b3ed170e0a6433685e5b5
ced888 <cedricdeleon70@gmail.com>
2025-07-19 19:46:30 -0600

    pokemon profile

--- portfolio-resume/src/components/sections/ProfileCard.tsx
+++ portfolio-resume/src/components/sections/ProfileCard.tsx
@@ -11,33 +11,21 @@ export function ProfileCard({ className = "", mouseSensitivity = 0 }: { classNam
       <CardBody className={`bg-gray-50 relative group/card 
         dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] 
         dark:bg-black dark:border-white/[0.2] border-black/[0.3] 
-        rounded-xl p-6 border ${className}`}>
+        rounded-xl border ${className}`}>
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
-        >
-          Cedric De Leon
-        </CardItem>
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-        >
-          Software Developer
-        </CardItem>
-        <CardItem
-          translateZ="50"
-          className="w-full mt-4"
+          position="absolute"
+          className="h-full w-full flex items-center justify-center"
         >
           <Image
-            src="/pics/profile_momiji.jpg"
+            src="/pics/Pokecardmaker.png"
             height="3000"
             width="1000"
-            className="h-90 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            className="h-full w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
           />
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
+        {/* <div className="flex justify-between items-center mt-20">
           <CardItem
             as="button"
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
@@ -52,7 +40,7 @@ export function ProfileCard({ className = "", mouseSensitivity = 0 }: { classNam
           >
             Sign up
           </CardItem>
-        </div>
+        </div> */}
       </CardBody>
     </CardContainer>
   );
